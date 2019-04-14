6a01201739165cbadc4497ed02573b5c421dd199
PVRA1819-1 <paulavaleriara@gmail.com>
2019-04-14 20:46:36 +0200

    Cambio textBox de notas por NumericUpDown

--- Practica6_1/Practica6_1/Form1.Designer.cs
+++ Practica6_1/Practica6_1/Form1.Designer.cs
@@ -31,23 +31,24 @@
             this.aluNombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // aluNombre
             // 
-            this.aluNombre.Location = new System.Drawing.Point(24, 44);
+            this.aluNombre.Location = new System.Drawing.Point(108, 41);
             this.aluNombre.Name = "aluNombre";
-            this.aluNombre.Size = new System.Drawing.Size(268, 20);
+            this.aluNombre.Size = new System.Drawing.Size(100, 20);
             this.aluNombre.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Location = new System.Drawing.Point(21, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 1;
@@ -56,23 +57,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(389, 28);
+            this.label2.Location = new System.Drawing.Point(21, 108);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Nota alumno";
             // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(392, 44);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(100, 20);
-            this.aluNota.TabIndex = 3;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 97);
+            this.label3.Location = new System.Drawing.Point(21, 175);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 4;
@@ -81,40 +75,48 @@
             // listaAlumnos
             // 
             this.listaAlumnos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.listaAlumnos.Location = new System.Drawing.Point(24, 113);
+            this.listaAlumnos.Location = new System.Drawing.Point(24, 202);
             this.listaAlumnos.Multiline = true;
             this.listaAlumnos.Name = "listaAlumnos";
             this.listaAlumnos.ReadOnly = true;
-            this.listaAlumnos.Size = new System.Drawing.Size(531, 97);
+            this.listaAlumnos.Size = new System.Drawing.Size(289, 97);
             this.listaAlumnos.TabIndex = 5;
             // 
             // button1
             // 
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.Location = new System.Drawing.Point(566, 41);
+            this.button1.Location = new System.Drawing.Point(246, 73);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.Size = new System.Drawing.Size(102, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "Guardar alumno";
+            this.button1.Text = "Añadir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(108, 106);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(100, 20);
+            this.aluNota.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(767, 263);
+            this.ClientSize = new System.Drawing.Size(445, 378);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listaAlumnos);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.aluNombre);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,10 +127,10 @@
         private System.Windows.Forms.TextBox aluNombre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox listaAlumnos;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
